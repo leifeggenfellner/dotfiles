097359200321befd91b5f1aa239bf3e4cd57d00a
leifeggenfellner <eggenfellner@protonmail.com>
2025-10-01 16:32:59 +0200

    feat: change default monitor to desc

--- modules/profiles/shitbox/default.nix
+++ modules/profiles/shitbox/default.nix
@@ -5,7 +5,7 @@
   imports = [ ../../default.nix ];
 
   # Home modules to load
-  program.hyprlock.defaultMonitor = "DP-5";
+  program.hyprlock.defaultMonitor = "desc:HP Inc. HP E45c G5 CNC50212K0";
 
   service = lib.mkMerge [
     {
