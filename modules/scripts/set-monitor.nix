fcace1097ff25839349dfaa157de5e05173d4e06
leifeggenfellner <eggenfellner@protonmail.com>
2026-01-15 10:47:04 +0100

    fix: monitor sripts

--- modules/scripts/set-monitor.nix
+++ modules/scripts/set-monitor.nix
@@ -1,14 +1,15 @@
 { pkgs, ... }:
-let
-  monitorOff = "hyprctl keyword monitor eDP-1,disable";
-  monitorOn = " hyprctl keyword monitor eDP-1,1920x1080@60,0x0,1";
-in
+
 pkgs.writeShellScriptBin "set-monitor" ''
-  if [ "$1" = "off" ]; then
-    ${monitorOff}
-  elif [ "$1" = "on" ]; then
-    ${monitorOn}
-  else
-    echo "Usage: set-monitor [on|off]"
-  fi
+  case "$1" in
+    on)
+      hyprctl keyword monitor "eDP-1,1920x1200@60,0x0,1"
+      ;;
+    off)
+      hyprctl keyword monitor "eDP-1,disable"
+      ;;
+    *)
+      echo "Usage: set-monitor [on|off]"
+      ;;
+  esac
 ''
