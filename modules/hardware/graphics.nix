7e69c5c8a32f83bf1a986fc22b78aae0a80dc9e4
leifeggenfellner <dev@zorvian.eu>
2026-06-02 12:21:45 +0200

    fix: remove duplicate package definition

--- modules/hardware/graphics.nix
+++ modules/hardware/graphics.nix
@@ -1,31 +1,8 @@
 _: {
   flake.nixosModules.hardware-graphics =
-    { config, pkgs, ... }:
+    { pkgs, ... }:
     {
-      hardware = {
-        graphics.enable = true;
-
-        nvidia = {
-          modesetting.enable = true;
-
-          powerManagement.enable = true;
-          powerManagement.finegrained = true;
-
-          open = false;
-          nvidiaSettings = true;
-          package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-          prime = {
-            sync.enable = false;
-            offload.enable = true;
-            offload.enableOffloadCmd = true;
-            intelBusId = "PCI:0:2:0";
-            nvidiaBusId = "PCI:1:0:0";
-          };
-        };
-      };
-
-      services.xserver.videoDrivers = [ "nvidia" ];
+      hardware.graphics.enable = true;
 
       environment.systemPackages = with pkgs; [
         vulkan-tools
