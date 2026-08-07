793b4ea7f1ad622a37d27d12fca30b817f4c584f
merrinx <gako.footwork856@passinbox.com>
2026-08-07 20:02:12 +0200

    chore(modules): remove scramgit service and input references

--- modules/services/scramgit.nix
+++
@@ -1,9 +0,0 @@
-{ inputs, ... }: {
-  flake.nixosModules.services-scramgit =
-    { pkgs, ... }:
-    {
-      environment.systemPackages = [
-        inputs.scramgit.defaultPackage.${pkgs.stdenv.hostPlatform.system}
-      ];
-    };
-}
